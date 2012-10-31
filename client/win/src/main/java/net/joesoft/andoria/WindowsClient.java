a92d0b45ca2b8bcd312b30608f59a8f57493ff6e
Josef Bauer <bauerjoe@gmx.de>
2012-11-01 00:52:05 +0100

    Added distribution package for Windows client

--- client/win/src/main/java/net/joesoft/andoria/WindowsClient.java
+++ client/win/src/main/java/net/joesoft/andoria/WindowsClient.java
@@ -2,7 +2,7 @@ package net.joesoft.andoria;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
-public class PCStarter {
+public class WindowsClient {
 	public static void main(String[] args) {
 		new LwjglApplication(new AndoriaGame(), "Andoria", 1280, 800, false);
 	}
